1820c0e7837d63d3d13bcb512fdb7b7aecebdbdd
Prince <princegoutam65101@gmail.com>
2024-05-11 12:50:59 +0530

    update my code

---
+++ While Loop Ques/index.js
@@ -0,0 +1,132 @@
+// Write a JavaScript program to print numbers from 1 to 50.
+
+// var num = 1;
+// while(num<=50){
+//     console.log(num);
+//     num=num+1;
+// }
+
+// Write a JavaScript program to print the multiplication table of 5 up to 10.
+
+// var i=1;
+// while(i<=10){
+//     let result=5*i;
+//     console.log(result);
+//     i++;
+// }
+
+// Write a JavaScript program to calculate the factorial of a given number
+
+// var fact=1;
+// var i=1;
+// while(i<=4){
+//     fact=fact*i;
+//     i++;
+// }
+// console.log(fact);
+
+// Write a JavaScript program to find the sum of digits of a positive integer
+
+// var sum=0;
+// var i=1;
+// while(i<=10){
+//     if(i%2==0){
+//         sum=sum+i;
+//     }
+//     i++;
+// }
+// console.log(sum);
+
+
+// Write a JavaScript program to find the sum of odd numbers from 1 to 50.
+
+// var sum=0;
+// var i=1;
+// while(i<=50){
+//     if(i%2!=0){
+//         sum=sum+i;
+//     }
+//     i++;
+// }
+// console.log(sum);
+
+
+// Write a JavaScript program to determine if a given number is prime or not.
+
+// var num = 7;
+// var count = 0;
+// var a = 1;
+// while (a<=num) {
+//     if (num % a == 0) {
+//         count++;
+//     }
+//     a++;
+// }
+// if (count == 2) {
+//     console.log("Number Is prime");
+// } else {
+//     console.log("Number Is Not Prime");
+// }
+
+// Write a JavaScript program to reverse a given number
+
+// var num = 244;
+// var rev = 0;
+// while (num != 0) {
+//     let digit = num % 10;
+//     rev = rev * 10 + digit;
+//     num = Math.floor(num / 10);
+// }
+// console.log(rev);
+
+
+// 8. Write a JavaScript program to find the sum of all prime numbers less than 100.
+
+// var num = 1;
+// var sum=0;
+// while (num <= 100) {
+//     var i = 1;
+//     var count = 0;
+//     while (i <= num) {
+//         if (num % i == 0) {
+//             count++;
+//         }
+//         i++;
+//     }
+//     if (count == 2) {
+//         sum=sum+num;
+//     }
+//     num++;
+// }
+// console.log(sum);
+
+
+
+// Write a JavaScript program to convert a decimal number to binary.
+
+
+
+// Write a JavaScript program to determine if a given string is a palindrome
+
+
+// var str="NAMAN";
+// var rev='';
+// var i = str.length-1;
+// while(i>=0){
+//     rev=rev+str[i];
+//     i--;
+// }
+// var orgStr=str.toLowerCase();
+// var revStr=rev.toLowerCase();
+
+// if(orgStr==revStr){
+//     console.log("String Is Palindrome");
+// }else{
+//     console.log("String is Not Palindrome");
+// }
+
+// Write a JavaScript program to find the greatest common divisor (GCD) of
+// two positive integers
+
+
+
